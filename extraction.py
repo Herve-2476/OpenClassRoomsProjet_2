85f0f94db5376ff995b041253d7940a6d72622b7
Herve_rasp <rv.monnier@free.fr>
2022-05-05 15:32:01 +0200

    création de la fonction pour extraire les livres dans une catégorie

--- extraction.py
+++ extraction.py
@@ -1,47 +1,70 @@
-#!usr/bin/env python
+#!usr/bin/env python3
 import requests
 from bs4 import BeautifulSoup
 import re
+import os
 
-domaine = "http://books.toscrape.com"
+DOMAINE = "http://books.toscrape.com"
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-r = requests.get(url).content
-soup = BeautifulSoup(r,"html.parser")
 
-#product_page_url
-data = {}
-data ["product_page_url"]=url
+def extraction_data_book(url):
 
-#universal_ product_code,price_excluding_tax,price_including_tax,number_available"
-entrees={
-        "UPC":"universal_ product_code",
-        "Price (excl. tax)":"price_excluding_tax",
-        "Price (incl. tax)":"price_including_tax",
-        "Availability":"number_available",
-        }
-for e in soup.table.find_all("tr"):
-    if e.th.string in entrees:
-        data [entrees[e.th.string]]=e.td.string
-data["number_available"] = int(re.findall(r'[\d]+', data["number_available"])[0])       
+    r = requests.get(url).content
+    soup = BeautifulSoup(r,"html.parser")
 
-#product_description
-e=soup.find_all("meta",{"name":"description"})[0]
-data["product_description"] = e.attrs["content"].strip()
+    #product_page_url
+    data = {}
+    data ["product_page_url"]=url
 
-#category,title
-e=soup.find_all("ul",class_="breadcrumb") [0]   
-l_e=e.find_all("li")
-data["category"] = l_e[2].a.string
-data["title"] = l_e[3].string
+    #universal_ product_code,price_excluding_tax,price_including_tax,number_available"
+    entrees={
+            "UPC":"universal_ product_code",
+            "Price (excl. tax)":"price_excluding_tax",
+            "Price (incl. tax)":"price_including_tax",
+            "Availability":"number_available",
+            }
+    for e in soup.table.find_all("tr"):
+        if e.th.string in entrees:
+            data [entrees[e.th.string]]=e.td.string
+    data["number_available"] = int(re.findall(r'[\d]+', data["number_available"])[0])       
 
-#review_rating
-for e in soup.find_all("p"):
-    if "class" in e.attrs and e["class"][0]=="star-rating":
-        data["review_rating"] = e["class"][1]
+    #product_description
+    e=soup.find_all("meta",{"name":"description"})[0]
+    data["product_description"] = e.attrs["content"].strip()
 
-#image_url
-data["image_url"] = domaine+soup.find_all("img")[0]["src"][5:]
+    #category,title
+    e=soup.find_all("ul",class_="breadcrumb") [0]   
+    l_e=e.find_all("li")
+    data["category"] = l_e[2].a.string
+    data["title"] = l_e[3].string
 
+    #review_rating
+    for e in soup.find_all("p"):
+        if "class" in e.attrs and e["class"][0]=="star-rating":
+            data["review_rating"] = e["class"][1]
 
-for k in data:
-    print (k,data[k])
+    #image_url
+    data["image_url"] = DOMAINE+soup.find_all("img")[0]["src"][5:]
+
+    return data
+
+url = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
+
+def extraction_list_books_in_category(url):
+    numero_page = 2
+    list_book=[]
+    while True:
+        r = requests.get(url).content
+        soup = BeautifulSoup(r,"html.parser")
+        #print(soup.prettify())
+        for e in soup.find_all("h3"):
+            list_book.append(DOMAINE+"/catalogue"+e.a["href"][8:])
+
+        if len(soup.find_all("li",class_="next"))==0:
+            break
+        url=os.path.dirname(url)+"/"+f"page-{numero_page}.html"
+    return list_book
+
+l=extraction_list_books_in_category(url)
+for e in l:
+    print (e)
