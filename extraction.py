d4070dfb0207c7f43fae2b7e3c5bfd49263d279f
Herve_rasp <rv.monnier@free.fr>
2022-05-05 16:54:57 +0200

    Mise en place de la recherche de toutes les catégories et création de tous les fichiers csv

--- extraction.py
+++ extraction.py
@@ -5,7 +5,7 @@ import re
 import os
 import csv
 
-DOMAINE = "http://books.toscrape.com"
+DOMAINE = "http://books.toscrape.com/"
 url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
 def extraction_data_book(url):
@@ -45,7 +45,7 @@ def extraction_data_book(url):
             data["review_rating"] = e["class"][1]
 
     #image_url
-    data["image_url"] = DOMAINE+soup.find_all("img")[0]["src"][5:]
+    data["image_url"] = DOMAINE+soup.find_all("img")[0]["src"][6:]
 
     return data
 
@@ -59,7 +59,7 @@ def extraction_list_books_in_category(url):
         soup = BeautifulSoup(r,"html.parser")
         #print(soup.prettify())
         for e in soup.find_all("h3"):
-            list_book.append(DOMAINE+"/catalogue"+e.a["href"][8:])
+            list_book.append(DOMAINE+"catalogue"+e.a["href"][8:])
 
         if len(soup.find_all("li",class_="next"))==0:
             break
@@ -67,13 +67,25 @@ def extraction_list_books_in_category(url):
         numero_page+= 1
     return list_book
 
-l=extraction_list_books_in_category(url)
-data_books_in_category=[]
-for url_book in l:
-    data_books_in_category.append(extraction_data_book(url_book))
+def creation_csv_by_category(url):
+    l=extraction_list_books_in_category(url)
+    data_books_in_category=[]
+    for url_book in l:
+        data_books_in_category.append(extraction_data_book(url_book))
 
-category="mystery"
-with open(f"csv/{category}.csv","w") as f:
-    data = csv.DictWriter(f,data_books_in_category[0].keys())
-    data.writeheader()
-    data.writerows(data_books_in_category)
+    category=url.split("/")[-2].split("_")[0]
+    with open(f"csv/{category}.csv","w") as f:
+        obj = csv.DictWriter(f,data_books_in_category[0].keys())
+        obj.writeheader()
+        obj.writerows(data_books_in_category)
+    print (f"Catégorie {category} traitée ({len(l)} livres).")
+
+url =   "http://books.toscrape.com/index.html"      
+r = requests.get(url).content
+soup = BeautifulSoup(r,"html.parser")
+
+#récupération des catégories, des livres de chaque catégories et écriture des fichiers csv
+#un fichier csv par catégorie avec les données indivuelles de chaque livre de cette catégories
+for e in soup.find_all("ul",class_="nav nav-list")[0].li.ul.find_all("li"):
+    url_category = DOMAINE+e.a["href"]
+    creation_csv_by_category(url_category)
